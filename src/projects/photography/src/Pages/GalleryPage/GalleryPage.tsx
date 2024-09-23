ca8886011f631ac1a30b0231a2a5c050898a2a3b
Julius Kirsch <Julius@skirsch.de>
2024-09-23 17:43:34 +0200

    feat: some test images on photography site

--- src/projects/photography/src/Pages/GalleryPage/GalleryPage.tsx
+++ src/projects/photography/src/Pages/GalleryPage/GalleryPage.tsx
@@ -6,10 +6,12 @@ export default function GalleryPage() {
         <div className={styles.galleryPage}>
             <div className={styles.headline}>Topics</div>
             <div className={styles.destinations}>
-                <ImageGalleryItem headline="Norway" image="./images/test1.png" allImages={[""]}/>
-                <ImageGalleryItem headline="Berlin" image="./images/test2.jpg" allImages={[""]}/>
-                <ImageGalleryItem headline="Neuschwanstein Castle" image="./images/test3.jpg" allImages={[""]}/>
-                <ImageGalleryItem headline="Marburg" image="./images/test4.jpg" allImages={[""]}/>
+                <ImageGalleryItem headline="Norway" image="./images/image1.jpg" allImages={[""]}/>
+                <ImageGalleryItem headline="Berlin" image="./images/image2.jpg" allImages={[""]}/>
+                <ImageGalleryItem headline="Neuschwanstein Castle" image="./images/image3.jpg" allImages={[""]}/>
+                <ImageGalleryItem headline="Lostplace" image="./images/image4.jpg" allImages={[""]}/>
+                <ImageGalleryItem headline="Lostplace" image="./images/image5.jpg" allImages={[""]}/>
+                <ImageGalleryItem headline="Crazy" image="./images/image6.jpg" allImages={[""]}/>
             </div>
         </div>
     );
